0d5f64ae69a6ca39e760677d348fa2cda0eb6647
Piyush Bhatia <Piyush_Bhatia@epam.com>
2020-12-30 12:19:44 +0530

    http bound channels

--- src/main/java/com/example/integration/demo/MessageServiceActivator.java
+++ src/main/java/com/example/integration/demo/MessageServiceActivator.java
@@ -1,13 +1,19 @@
 package com.example.integration.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
+
+import com.example.integration.demo.model.CachingServiceImpl;
 
 @MessageEndpoint
 public class MessageServiceActivator {
 
-	@ServiceActivator(inputChannel = "transformerOutputChannel")
+	@Autowired
+	CachingServiceImpl cachingServiceImpl;
+	@ServiceActivator(inputChannel = "aggregatorOutputChannel")
 	public void getMessage(String message) {
+		cachingServiceImpl.updateDataInMap(message);
 		System.out.println(message);
 	}
 
